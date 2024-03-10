9b7b85d728b111fa21e532c83ac2dac27b939ab8
waseemjanyaro <waseemjanyaro@gmail.com>
2024-03-10 18:59:32 +0500

    create complete cart model

--- lib/cart_model.dart
+++ lib/cart_model.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/rendering.dart';
+
 class Cart {
   late int? id;
   late final String? productId;
@@ -7,13 +9,31 @@ class Cart {
   late final String? unitTag;
   late final String? image;
 
-  Cart({
-    required this.id,
-    required this.productId,
-    required this.productName,
-    required this.ProductPrice,
-    required this.initialPrice,
-    required this.unitTag,
-    required this.image
-  })
+  Cart(
+      {required this.id,
+      required this.productId,
+      required this.productName,
+      required this.ProductPrice,
+      required this.initialPrice,
+      required this.unitTag,
+      required this.image});
+  Cart.fromMap(Map<dynamic, dynamic> res)
+      : id = res['id'],
+        productId = res['productId'],
+        productName = res['productName'],
+        ProductPrice = res['ProductPrice'],
+        initialPrice = res['initialPrice'],
+        unitTag = res['unitTag'],
+        image = res['image'];
+  Map<String, Object?> toMap() {
+    return {
+      'id': id,
+      'productId': productId,
+      "productName": productName,
+      "initialPrice": initialPrice,
+      "ProductPrice": ProductPrice,
+      "unitTag": unitTag,
+      "image": unitTag
+    };
+  }
 }
