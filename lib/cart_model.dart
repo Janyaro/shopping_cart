c01ccb0761a865540c22a17924c1bcc7ac81dd11
waseemjanyaro <waseemjanyaro@gmail.com>
2024-03-10 13:00:06 +0500

    create cart model

---
+++ lib/cart_model.dart
@@ -0,0 +1,19 @@
+class Cart {
+  late int? id;
+  late final String? productId;
+  late final String? productName;
+  late final int? initialPrice;
+  late final int? ProductPrice;
+  late final String? unitTag;
+  late final String? image;
+
+  Cart({
+    required this.id,
+    required this.productId,
+    required this.productName,
+    required this.ProductPrice,
+    required this.initialPrice,
+    required this.unitTag,
+    required this.image
+  })
+}
